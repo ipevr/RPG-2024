27641bfe542cae95058125316b5d8149892b84d2
Ingo Peters <ingo.peters2@gmx.de>
2024-11-27 19:58:42 +0100

    feat: preparing enemy chase behaviour

---
+++ Assets/Scripts/RPG/Control/AIController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] private float chaseDistance = 5f;
+
+        private GameObject player;
+
+        private void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void Update()
+        {
+            ProcessPlayerChaseBehaviour();
+        }
+
+        private void ProcessPlayerChaseBehaviour()
+        {
+            if (IsInChaseDistance(player))
+            {
+                Debug.Log($"{gameObject.name} is chasing.");
+            }
+        }
+
+        private bool IsInChaseDistance(GameObject other)
+        {
+            return Vector3.Distance(other.transform.position, transform.position) < chaseDistance;
+        }
+    }
+}
