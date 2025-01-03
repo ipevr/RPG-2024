96f94d4df852925e33e9657f90144c9516e4e49f
Ingo Peters <ingo.peters2@gmx.de>
2025-01-03 18:39:54 +0100

    fix: Refactoring of pickup sound feat: pickup respawn

---
+++ Assets/Scripts/RPG/Combat/PickupSound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class PickupSound : MonoBehaviour
+    {
+        [SerializeField] private AudioClip pickupClip;
+
+        public void Start()
+        {
+            GetComponent<AudioSource>().PlayOneShot(pickupClip);
+            
+            Destroy(gameObject, pickupClip.length);
+        }
+        
+        
+    }
+}
--- Assets/Scripts/RPG/Combat/WeaponPickUp.cs
+++ Assets/Scripts/RPG/Combat/WeaponPickUp.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace RPG.Combat
 {
     public class WeaponPickUp : MonoBehaviour
     {
         [SerializeField] private Weapon weapon;
-        [SerializeField] private AudioClip pickupClip;
+        [SerializeField] private PickupSound pickupSound;
+        [SerializeField] private float respawnTime = 5f;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -13,18 +15,27 @@ namespace RPG.Combat
             
             other.gameObject.GetComponent<Fighter>().EquipWeapon(weapon);
             
-            GetComponent<AudioSource>().PlayOneShot(pickupClip);
-            
-            DisableChildren();
-            Destroy(gameObject, pickupClip.length);
-        }
-        
-        private void DisableChildren()
-        {
-            var children = transform.childCount;
-            for (var i = 0; i < children; i++)
+            if (pickupSound)
             {
-                transform.GetChild(i).gameObject.SetActive(false);
+                Instantiate(pickupSound, transform.position, Quaternion.identity);
+            }
+            
+            StartCoroutine(HideForSeconds(respawnTime));
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool status)
+        {
+            GetComponent<Collider>().enabled = status;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(status);
             }
         }
     }
